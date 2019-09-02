a698aaaca017ba0ec28b7ccd394eb94095bc780f
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-09-02 00:32:57 -0300

    feat(OrgIdInput): Add functionality

--- src/components/OrigIdInput.js
+++ src/components/OrigIdInput.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Row, Col, Form, Button } from '@windingtree/wt-ui-react';
-class OrgIdInput extends Component {
 
-  render() {
-    return (
-      <Container>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Control placeholder="ORG.ID Address" type="text"/>
-            </Col>
-            <Col md={1} className="mr-1">
-              <Button variant="primary" type="submit">
-                GO
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </Container>
-    );
-  }
-}
+const OrgIdInput = ({ value, onChange }) =>(
+  <Container className="mb-1">
+    <Form>
+      <Row>
+        <Col>
+          <Form.Control placeholder="ORG.ID Address" type="text" onChange={onChange} value={value}/>
+        </Col>
+        <Col md={1} className="mr-1">
+          <Button variant="primary" type="submit" href={`/orgid/${value}`}>
+            GO
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  </Container>
+);
 
 export default OrgIdInput;
   
