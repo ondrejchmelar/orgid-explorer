993f98f32e8a8614c3b8d6dd920802fd82a7af89
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-09-02 00:33:39 -0300

    feat(Route): Add details route and redirect

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import List from './List';
+import OrgIdDetails from './OrgIdDetails'
 import Page404 from './components/Page404';
 
 const Routes = () => (
   <Switch>
+    <Route path={["/orgid/:id"]} exact component={OrgIdDetails} />
+    <Redirect exact from={["/orgid","/orgid/"]} to='/'/>
     <Route path="/" exact component={List} />
     <Route component={Page404} />
   </Switch>
 );
-
 
 export default Routes;
