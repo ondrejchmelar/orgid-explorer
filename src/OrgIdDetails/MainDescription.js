1c2e2b58a2c5f0c6471691bb1ead4e62b4b24b50
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-09-05 17:56:49 -0300

    feat(Details add message for inexistent orgid):

--- src/OrgIdDetails/MainDescription.js
+++ src/OrgIdDetails/MainDescription.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+const InvalidOrgId = ({ id }) => (
+  <div className="align-self-center text-center">
+    <h3>ORG.ID {id} not found</h3>
+    <h4>
+      IF you just cretead an new ORG.ID, have in mind that scraping task is scheduled at 
+      2am and 2pm GMT.
+    </h4>
+  </div>
+)
 
-const MainDescription = ({
-  directory = 'Hotels',
-  id = '0xa5ed9797ec02fe3a5af456590863d8f0fe1196a0',
-  created = '3 weeks ago',
-  updated = '1 year ago'
-}) => (
+const MainDescription = ({ directory, id, created, updated }) => {
+  if (!created) return <InvalidOrgId id={id} />
+  return (
   <div className="align-self-center text-center">
     <h4>ORG.ID in {directory} directory</h4>
     <h3>{id}</h3>
     <p>Created: {created} Updated:{updated} </p>
   </div>
-);
+)};
 
 export default MainDescription;
