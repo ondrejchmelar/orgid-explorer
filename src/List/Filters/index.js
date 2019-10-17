90f4ab909548a611d90d6100340c5195f51f4fdd
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-10-17 16:46:41 -0300

    feat(Filters): Display env filter

--- src/List/Filters/index.js
+++ src/List/Filters/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
 import Autocomplete from 'react-google-autocomplete';
 import { Container, Row, Col, Form, Dropdown, Button } from '@windingtree/wt-ui-react';
-import { directories } from './directories';
+import { directories, environments } from './filterInfo';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -11,17 +11,17 @@ class Filters extends Component {
   render() {
     const { 
       startDate, endDate, onStartDateChange, onEndDateChange, onDirectoryChange, selectedDirectory,
-      onApply, onLocationChange,
+      onApply, onLocationChange, onEnvironmentChange, selectedEnvironment,
     } = this.props;
 
     return (
       <Container className="mt-1">
         <Form>
           <Row>
-            <Col md={1} xs={3} className="align-self-center font-weight-bold mt-1">
-              Directory
+            <Col md={1} xs={4} className="align-self-center font-weight-bold mt-1">
+              Directory:
             </Col>
-            <Col md={2} xs={3} className="align-self-center mt-1">
+            <Col md={4} xs={7} className="mt-1">
               <Dropdown
                 onSelect={onDirectoryChange}
               >
@@ -40,31 +40,53 @@ class Filters extends Component {
                 </Dropdown.Menu>
               </Dropdown>
             </Col>
-          </Row>
-          <Row>
-            <Col  md={1} className="align-self-center font-weight-bold mt-1">
-              Filters
+            <Col md="auto" xs={4} className="align-self-center font-weight-bold mt-1">
+              Environment:
             </Col>
-            <Col md={4} className="align-self-center mt-1">
+            <Col md={4} xs={7} className="align-self-center mt-1">
+              <Dropdown
+                onSelect={onEnvironmentChange}
+              >
+                <Dropdown.Toggle variant="light" id="dropdown-basic">
+                {selectedEnvironment}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {
+                    environments.map(environment => (
+                      <Dropdown.Item key={environment} eventKey={environment}>
+                        {environment}
+                      </Dropdown.Item>
+                    ))
+                  }
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+          </Row>
+          <Row className="mt-1">
+            <Col  md={1} className="align-self-center font-weight-bold mt-1">
+              Filters:
+            </Col>
+            <Col md={4} className="align-self-center mt-md-1">
               <Autocomplete
                 onPlaceSelected={onLocationChange}
                 types={['(cities)']}
               />
             </Col>
-            <Col md={1} className="align-self-center mt-1">Creation date</Col>
-            <Col className="align-self-center mt-1">
+            <Col md="auto" className="align-self-center mt-1">Creation date</Col>
+            <Col className="align-self-center mt-md-1">
               <DatePicker
                 selected={startDate}
                 onChange={onStartDateChange}
               />
             </Col>
-            <Col className="align-self-center mt-1">
+            <Col className="align-self-center mt-md-1">
               <DatePicker
                 selected={endDate}
                 onChange={onEndDateChange}
               />
             </Col>
-            <Col md={2} className="align-self-center mt-1">
+            <Col md={2} className="align-self-center mt-2 mt-md-1">
               <Button
                 onClick={onApply} 
                 variant="dark" 
